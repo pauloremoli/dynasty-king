17f9528e570d642ba068eb724207242265c6ad90
Paulo Remoli <paulo002@gmail.com>
2022-05-06 15:50:22 +0200

    Remove debug message

--- app/routes/tools.tsx
+++ app/routes/tools.tsx
@@ -1,22 +1,13 @@
-import { LoaderFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
 import React from "react";
-import { getUsers } from "~/models/user.server";
 
-export const loader: LoaderFunction = async ({ request }) => {
-  const data = await getUsers();
-
-  return { data };
-};
 
 const Tools = () => {
 
-  const { data } = useLoaderData();
   return (
     <>
       <div className="flex flex-col w-full h-full items-center pt-24 text-white">
         <h1 className="text-4xl font-bold text-center pb-20">Tools</h1>
-        <pre>Users: {JSON.stringify(data, null, 2)}</pre>
+        <h2>Work in progress...</h2>
       </div>
     </>
   );
